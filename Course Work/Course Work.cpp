b7c62433500beabd5d55587eaae569a1d0f72906
gladk <gladk@XTRV>
2023-12-25 00:14:37 +0300

    Добавьте файлы проекта.

---
+++ Course Work/Course Work.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include "Lab1.cpp"
+#include "Lab2.cpp"
+#include "Lab3.cpp"
+#include "Lab4.cpp"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    system("cls");
+    int lab;
+    cout << "Веберите номер практической работы, которую хотите выбрать(1 - 4):\n\n"
+        << "0 - Выход из программы.\n\n";
+    cin >> lab;
+    system("cls");
+    switch (lab)
+    {
+    case 1:
+        menu1();
+        main();
+        break;
+    case 2:
+        menu2();
+        main();
+        break;
+    case 3:
+        menu3();
+        main();
+        break;
+    case 4:
+        menu4();
+        main();
+        break;
+    case 0:
+        exit(1);
+        break;
+    default:
+        main();
+        break;
+    }
+    return 0;
+}
---
+++ Course Work/Lab1.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void sizes()
+{
+    setlocale(LC_ALL, "RU");
+    cout << "Задание №1" << "\n\n";
+    cout << "Размер int: " << sizeof(int) << "\n"
+        << "Размер short int: " << sizeof(short int) << "\n"
+        << "Размер long int: " << sizeof(long int) << "\n"
+        << "Размер float: " << sizeof(float) << "\n"
+        << "Размер double: " << sizeof(double) << "\n"
+        << "Размер long double: " << sizeof(long double) << "\n"
+        << "Размер char: " << sizeof(char) << "\n"
+        << "Размер bool: " << sizeof(bool) << "\n\n"
+        << "Нажмите любую клавишу, чтобы вернуться в главное меню";
+    _getch();
+    system("cls");
+}
+
+void integerToBinary()
+{
+    cout << "Задание №2: " << "\n\n";
+    int value;
+    cout << "Введите число типа int:\n\n";
+    cin >> value;
+    int order = sizeof(value) * 8;
+    unsigned int mask = 1 << order - 1;
+    for (int i = 0; i < order; i++) {
+        putchar((value & mask) ? '1' : '0');
+        mask >>= 1;
+
+        if (i % 8 == 0)
+            putchar(' ');
+    }
+    cout << "\n\nНажмите любую клавишу, чтобы вернуться в главное меню";
+    _getch();
+    system("cls");
+}
+
+void floatToBinary()
+{
+    cout << "\n\n" << "Задание №3" << "\n\n";
+    union
+    {
+        int intA;
+        float floatB;
+    };
+    cout << "Введите число типа float:\n\n";
+    cin >> floatB;
+    int order = sizeof(float) * 8;
+    unsigned int mask = 1 << order - 1;
+    for (int i = 0; i < order; i++) {
+        putchar((intA & mask) ? '1' : '0');
+        mask >>= 1;
+        if (i == 0 || i == 8)
+            putchar(' ');
+    }
+    cout << "\n\nНажмите любую клавишу, чтобы вернуться в главное меню";
+    _getch();
+    system("cls");
+}
+
+void doubleToBinary()
+{
+    cout << "\n\n" << "Задание №4" << "\n\n";
+    union
+    {
+        int doubleM[2];
+        double doubleD;
+    };
+    int order = sizeof(double) * 8;
+    unsigned int mask = 1 << order - 1;
+    cout << "\n\nВведите число типа double:\n\n";
+    cin >> doubleD;
+    for (int i = 1; i >= 0; i--)
+    {
+        for (int j = 0; j < order / 2; j++)
+        {
+            putchar((doubleM[i] & mask) ? '1' : '0');
+            mask >>= 1;
+            if (i == 1 && (j == 0 || j == 11))
+                putchar(' ');
+        }
+        mask = 1 << order - 1;
+    }
+    cout << "\n\nНажмите любую клавишу, чтобы вернуться в главное меню";
+    _getch();
+    system("cls");
+}
+
+void menu()
+{
+    setlocale(LC_ALL, "RU");
+    int mainMenu;
+    {
+        cout << "Какой пункт вы хотите выбрать?\n"
+            << "1 - Cколько байтов на вашем компьютере отводится под различные типы данных\n"
+            << "2 - Двоичное представление в памяти числа типа int\n"
+            << "3 - Двоичное представление в памяти числа типа float\n"
+            << "4 - Двоичное представление в памяти числа типа double\n"
+            << "5 - Выход\n\n";
+    }
+    cin >> mainMenu;
+
+    switch (mainMenu)
+    {
+    case 1:
+        sizes();
+        menu();
+        break;
+    case 2:
+        integerToBinary();
+        menu();
+        break;
+    case 3:
+        floatToBinary();
+        menu();
+        break;
+    case 4:
+        doubleToBinary();
+        menu();
+        break;
+    case 5:
+        break;
+    default:
+        cout << "Ошибка! Попробуйте снова";
+        _getch();
+        system("cls");
+        menu();
+        break;
+
+    }
+}
+
+void menu1()
+{
+    menu();
+}
